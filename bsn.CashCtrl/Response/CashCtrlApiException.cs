6337c772871c3b925f061e326b0f90e9fd476e9d
Arsène von Wyss <arsene.vonwyss@sirius.ch>
2024-01-02 20:53:29 +0100

    Fix for NullReferenceException in CashCtrlApiException construtor

--- bsn.CashCtrl/Response/CashCtrlApiException.cs
+++ bsn.CashCtrl/Response/CashCtrlApiException.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Runtime.Serialization;
 
@@ -8,10 +8,14 @@ namespace bsn.CashCtrl.Response {
 		public CashCtrlApiException(string message): base(message) { }
 
 		public CashCtrlApiException(string message, UpdateResponse.Error[] errors): base(
-				string.IsNullOrEmpty(message) && errors.Length == 1 && string.IsNullOrEmpty(errors[0].Field)
-						? errors[0].Message
-						: $"{(string.IsNullOrEmpty(message) ? "The operation failed due to field errors." : message)} {string.Join("; ", errors.Select(e => $"{e.Field}: {e.Message}"))}") {
-			Errors = errors;
+				errors?.Length > 0
+						? string.IsNullOrEmpty(message) && errors.Length == 1 && string.IsNullOrEmpty(errors[0].Field)
+								? errors[0].Message
+								: $"{(string.IsNullOrEmpty(message) ? "The operation failed due to field errors." : message)} {string.Join("; ", errors.Select(e => $"{e.Field}: {e.Message}"))}"
+						: string.IsNullOrEmpty(message)
+								? "Unknown API error"
+								: message) {
+			this.Errors = errors ?? Array.Empty<UpdateResponse.Error>();
 		}
 
 		public CashCtrlApiException(string message, Exception innerException): base(message, innerException) { }
